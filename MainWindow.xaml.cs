a76fc57e40f79a42bf919ae2e65c66c1c1fe8474
Entity233 <2966388213@qq.com>
2023-03-08 23:44:03 +0800

    3.8

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using 替换关键词.Model;
+
+namespace 替换关键词 {
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            // init
+            JsonUtils.Init();
+            KeyWrodList.ItemsSource = JsonUtils.Ks;
+
+        }
+
+        /// <summary>
+        /// 删除元素
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void List_Delete_Click(object sender, RoutedEventArgs e) {
+            var item = (sender as Button).Tag as string;
+            if (item != null) {
+                MessageBox.Show(item);
+            }
+        }
+    }
+}
---
+++ Model/JsonUtils.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace 替换关键词.Model {
+    class KeyWord {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+    static class JsonUtils
+    {
+        public static List<KeyWord> Ks = null;
+        public static string path = "KeyWordLists.json";
+
+        public static void OpenFile() {
+            using (var reader = File.OpenText(path)) {
+                var T = (JArray)JToken.ReadFrom(new JsonTextReader(reader));
+                Ks = (List<KeyWord>)T.ToObject(typeof(List<KeyWord>));
+            }
+        }
+        public static void WriteFile() {
+            var json = JsonConvert.SerializeObject(Ks);
+            File.WriteAllText(path, json);
+        }
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public static void Init() {
+            if (File.Exists(path)) {
+                OpenFile();
+            }
+            else {
+                var data = new List<KeyWord>() 
+                {
+                    new KeyWord() {
+                        Key = "效果",
+                        Value = "成效"
+                    },
+                    new KeyWord() {
+                        Key = "最",
+                        Value = "好"
+                    }
+                };
+                // 写出默认值
+                WriteFile();
+            }
+        }
+    }
+}
