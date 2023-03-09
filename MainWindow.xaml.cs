4903269bb4536d06298e757e0cb1f56c26fd37b8
Entity-Now <2966388213@qq.com>
2023-03-09 09:59:58 +0800

    3.9

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -19,11 +21,17 @@ namespace 替换关键词 {
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow : Window {
+        public ObservableCollection<KeyWord> SourceList = new ObservableCollection<KeyWord>();
         public MainWindow() {
             InitializeComponent();
+            DataContext = this;
+            KeyWordList.ItemsSource = SourceList;
             // init
             JsonUtils.Init();
-            KeyWrodList.ItemsSource = JsonUtils.Ks;
+            foreach (KeyWord keyWord in JsonUtils.Ks)
+            {
+                SourceList.Add(keyWord);
+            }
 
         }
 
@@ -33,9 +41,71 @@ namespace 替换关键词 {
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void List_Delete_Click(object sender, RoutedEventArgs e) {
-            var item = (sender as Button).Tag as string;
-            if (item != null) {
-                MessageBox.Show(item);
+            var item = (sender as Button).Tag as KeyWord;
+            if (item != null)
+            {
+                var ms = MessageBox.Show("确定要删除吗", "警告", MessageBoxButton.YesNo);
+               
+                if (ms == MessageBoxResult.Yes)
+                {
+                    JsonUtils.remove(item);
+                    SourceList.Remove(item);
+                }
+            }
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            if (KeyWord.Text.Length <= 0)
+            {
+                MessageBox.Show("请输入关键词");
+                return;
+            }
+            if (Content.Text.Length <= 0)
+            {
+                MessageBox.Show("请输入要替换的值");
+                return;
+            }
+            var data = new KeyWord()
+            {
+                Key = KeyWord.Text,
+                Value = Content.Text
+            };
+            JsonUtils.add(data);
+            SourceList.Add(data);
+            // clean
+            KeyWord.Text = "";
+            Content.Text = "";
+        }
+
+        private void KeyWordList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selector = (sender as ListBox).SelectedItem as KeyWord;
+            if (selector != null)
+            {
+                KeyWord.Text = selector.Key;
+                Content.Text = selector.Value;
+            }
+        }
+
+        private void update_Click(object sender, RoutedEventArgs e)
+        {
+            string Text = frontText.Text;
+            if (Text.Length > 0)
+            {
+                foreach (var item in JsonUtils.Ks)
+                {
+                    string[] splitText = item.Value.Split(new char[] { ',' });
+                    string newString = splitText[new Random().Next(splitText.Length)];
+                    // 随机选择一个
+                    Text = Text.Replace(item.Key, newString);
+
+                }
+                behindText.Text = Text;
+            }
+            else
+            {
+                MessageBox.Show("请输入要替换的文本");
             }
         }
     }
--- Model/JsonUtils.cs
+++ Model/JsonUtils.cs
@@ -7,13 +7,15 @@ using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
 using System.IO;
 using Newtonsoft.Json;
+using System.Xml.Linq;
+using System.Windows;
 
 namespace 替换关键词.Model {
-    class KeyWord {
+    public class KeyWord {
         public string Key { get; set; }
         public string Value { get; set; }
     }
-    static class JsonUtils
+    public static class JsonUtils
     {
         public static List<KeyWord> Ks = null;
         public static string path = "KeyWordLists.json";
@@ -24,11 +26,69 @@ namespace 替换关键词.Model {
                 Ks = (List<KeyWord>)T.ToObject(typeof(List<KeyWord>));
             }
         }
-        public static void WriteFile() {
-            var json = JsonConvert.SerializeObject(Ks);
+        public static void WriteFile(List<KeyWord> data = null) {
+            List<KeyWord> temp = Ks;
+            if (data != null)
+            {
+                temp = data;
+            }
+            var json = JsonConvert.SerializeObject(temp);
             File.WriteAllText(path, json);
         }
-
+        public static void update(KeyWord item)
+        {
+            var data = Ks.FirstOrDefault(I => I == item);
+            if (data != null)
+            {
+                data = item;
+            }
+            WriteFile();
+        }
+        public static void update(string name, string value)
+        {
+            var data = Ks.FirstOrDefault(I => I.Key == name);
+            if (data != null)
+            {
+                data.Key = name;
+                data.Value = value;
+            }
+            WriteFile();
+        }
+        public static void add(KeyWord item)
+        {
+            var data = Ks.FirstOrDefault(I => I == item);
+            if (data != null)
+            {
+                update(item);
+                return;
+            }
+            Ks.Add(item);
+            WriteFile();
+        }
+        public static void add(string name, string value)
+        {
+            var data = Ks.FirstOrDefault(I => I.Key == name);
+            if (data != null)
+            {
+                update(name,value);
+                return;
+            }
+            Ks.Add(new KeyWord()
+            {
+                Key = name, Value = value
+            });
+            WriteFile();
+        }
+        public static void remove(string name)
+        {
+            Ks.Remove(new KeyWord() { Key = name});
+            WriteFile();
+        }
+        public static void remove(KeyWord item)
+        {
+            Ks.Remove(item);
+            WriteFile();
+        }
         /// <summary>
         /// 初始化
         /// </summary>
@@ -49,7 +109,7 @@ namespace 替换关键词.Model {
                     }
                 };
                 // 写出默认值
-                WriteFile();
+                WriteFile(data);
             }
         }
     }
